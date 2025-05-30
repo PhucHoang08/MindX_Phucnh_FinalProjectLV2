4896393f5471f3b918e15b718834f2e3e1e55f40
hello <nhoangphuc960@gmail.com>
2025-05-30 18:32:19 +0700

    update profile

--- WEB-[TC-CIJS103]/src/components/profile/Profile.jsx
+++ WEB-[TC-CIJS103]/src/components/profile/Profile.jsx
@@ -5,12 +5,15 @@ import { useState } from 'react'
 
 function Profile() {
   const navigate = useNavigate();
-  const [showModal,setShowModal] = useState(false)
   const user = JSON.parse(localStorage.getItem("user"))
   console.log(user)
   const handleLogout = () => {
     localStorage.clear();
     navigate("/login");
+  };
+
+  const handleBack = () => {
+    navigate(-1);
   };
 
   return (
@@ -30,12 +33,12 @@ function Profile() {
           <h5 className="mb-1 text-xl font-medium text-black">{user?.fullName}</h5>
           <span className="text-sm text-gray-500 dark:text-gray-400">{user?.email}</span>
           <div className="flex mt-4 md:mt-6">
-            <Link
-              to="/"
+            <button
+              onClick={handleBack}
               className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               BACK
-            </Link>
+            </button>
             <button onClick={handleLogout}
               className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
             >
