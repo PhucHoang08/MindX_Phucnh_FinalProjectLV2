d384753022ed4210ea37246a74105126e7418dcc
hello <nhoangphuc960@gmail.com>
2025-05-30 20:09:37 +0700

    update

--- WEB-[TC-CIJS103]/src/main.jsx
+++ WEB-[TC-CIJS103]/src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-
+import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 
@@ -10,6 +10,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
 
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 
 )
